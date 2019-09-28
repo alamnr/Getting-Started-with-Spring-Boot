b3c90664f2b6f645e31f61e1b166cb87b992fad5
alamnr <alamnr@gmail.com>
2019-09-28 19:08:04 +0600

    Additional bean configuration

--- src/main/java/com/oreilly/boot/Application.java
+++ src/main/java/com/oreilly/boot/Application.java
@@ -1,21 +1,37 @@
 package com.oreilly.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/*@EnableAutoConfiguration
+//@Import(MyConfiguration.class)
+@ComponentScan()
+*/
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class Application {
 	
 	@Value("${name}")
-	String name;
+	private String name;
 	
+	private String message;
+	
+	@Autowired
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World " +  name;
+		return "Hello World " +  name  +   message;
 	}
 
 	public static void main(String[] args) throws Exception {
